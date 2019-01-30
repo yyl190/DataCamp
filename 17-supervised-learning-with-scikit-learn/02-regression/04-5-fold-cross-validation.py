102d00fa787a5911242cd548af186acccd81400f
yyl190 <34369217+yyl190@users.noreply.github.com>
2019-01-30 23:21:18 +0800

    Update 04-5-fold-cross-validation.py

--- 17-supervised-learning-with-scikit-learn/02-regression/04-5-fold-cross-validation.py
+++ 17-supervised-learning-with-scikit-learn/02-regression/04-5-fold-cross-validation.py
@@ -9,26 +9,19 @@ R
  as the metric of choice for regression. Since you are performing 5-fold cross-validation, the function will return 5 scores. Your job is to compute these 5 scores and then take their average.
 
 The DataFrame has been loaded as df and split into the feature/target variable arrays X and y. The modules pandas and numpy have been imported as pd and np, respectively.
-
-INSTRUCTIONS
-100XP
-Import LinearRegression from sklearn.linear_model and cross_val_score from sklearn.model_selection.
-Create a linear regression regressor called reg.
-Use the cross_val_score() function to perform 5-fold cross-validation on X and y.
-Compute and print the average cross-validation score. You can use NumPy's mean() function to compute the average.
 '''
-# Import necessary modules
+# Import the necessary modules
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import roc_auc_score
 
-# Compute predicted probabilities: y_pred_prob
-y_pred_prob = logreg.predict_proba(X_test)[:,1]
+# Create a linear regression object: reg
+reg = LinearRegression()
 
-# Compute and print AUC score
-print("AUC: {}".format(roc_auc_score(y_test, y_pred_prob)))
+# Compute 5-fold cross-validation scores: cv_scores
+cv_scores = cross_val_score(reg, X, y, cv=5)
 
-# Compute cross-validated AUC scores: cv_auc
-cv_auc = cross_val_score(logreg, X, y, cv=5, scoring='roc_auc')
+# Print the 5-fold cross-validation scores
+print(cv_scores)
 
-# Print list of AUC scores
-print("AUC scores computed using 5-fold cross-validation: {}".format(cv_auc))
+print("Average 5-Fold CV Score: {}".format(np.mean(cv_scores)))
+
